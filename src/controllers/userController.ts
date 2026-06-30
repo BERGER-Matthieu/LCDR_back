147eda849a7794d9ff231e35bcd02a7b818d0a4a
BERGER-Matthieu <matthieu.berger.27430@gmail.com>
2026-06-30 16:29:24 +0200

    + updateUser, WARNING: not working everytimes, might be on mongo wsl side

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,13 +1,27 @@
 import User from "../models/userModel";
 
-export const readUsers = async () => {
+export const readUsers = async (id? : string) => {
     try {
-        return await User.find();
+        if (id) {
+            return await User.findById(id);
+        } else {
+            return await User.find();
+        }
     } catch (error) {
         console.error("Error fetching users:", error);
         return [];
     }
 };
+
+export const updateUser = async (id: string, body: User) => {
+    try {
+        return await User.findByIdAndUpdate(id, body);
+    } catch (error) {
+        console.error("Error updating user:", error);
+        throw error;
+    }
+};
+
 
 export const createUser = async (body: User) => {
     try {
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,17 +1,27 @@
 import { t } from "elysia";
-import { readUsers, createUser } from "../controllers/userController";
+import { readUsers, createUser, updateUser } from "../controllers/userController";
 
 export default (app: any) => {
     app.get("/users", async () => {
         return await readUsers();
-    })
+    }); 
 
-    app.post("/users", async ({body}: {body: User}) => {
-        try {
-            return await createUser(body);
-        } catch (error) {
-            throw error;
-        }},
+    app.get("/users/:id", async ({params: {id}} : {params: {id: string}}) => {
+        return await readUsers(id);
+    });
+
+    app.put("/users/:id", async ({params: { id }, body }: { params: { id: string }; body: User }) => {
+        return await updateUser(id, body)
+    });
+
+    app.post("/users", async ({body}: {body: User}) => 
+            {
+            try {
+                return await createUser(body);
+            } catch (error) {
+                throw error;
+            }
+        },
         {
             body: t.Object({
                 name: t.String(),
