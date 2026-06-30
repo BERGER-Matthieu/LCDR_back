c1fe9062e19dd326404351b6d51033653e96625c
BERGER-Matthieu <matthieu.berger.27430@gmail.com>
2026-06-30 16:57:44 +0200

    + .env PORT variable

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import { Elysia } from "elysia";
 import { connectDB } from "./db";
 import userRoutes from "./routes/userRoutes";
 
+const PORT : number = parseInt(process.env.PORT) ?? 3001
+
 await connectDB();
 
 const app = new Elysia();
 
 userRoutes(app);
 
-app.listen(3000);
+app.listen(PORT);
 console.log(`Elysia running at ${app.server?.hostname}:${app.server?.port}`);
